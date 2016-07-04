1e98468b9c4cdb8196691f0ac1f946cb9967eb61
qualiu <qualiu@microsoft.com>
2016-07-04 18:34:48 +0800

    Update pskill.bat

--- tools/pskill.bat
+++ tools/pskill.bat
@@ -1,23 +1,15 @@
 :: kill process by id
 @SETLOCAL
 ::@echo %* | findstr "[A-Za-z]">nul && call :KillByRegex %* || call :KillByPID %*
-@SET "NotNumber=" & for /f "delims=0123456789 " %%a in ("%*") do set NotNumber=%%a
+@SET "NotNumber=" & for /f "delims=0123456789 " %%a in ("%*") do @set NotNumber=%%a
 @if defined NotNumber (
-    @call :KillByRegex %*
-) else (       
-    @call :KillByPID %*
-)
-
-:KillByRegex
     @echo Kill process by regex : %*
-    for /F "tokens=2" %%p in ('psall -it %* -PAC') do @call set "xpids=/pid %%p %%xpids%%"
-    @if not "%xpids%" == "" taskkill /f %xpids%
-    @goto :End
-    
-:KillByPID
+    for /F "tokens=2" %%p in ('@psall -it %* -PAC') do @call set "xpids=/pid %%p %%xpids%%"
+    if not "%xpids%" == "" taskkill /f %xpids%
+) else (
     @echo Kill process by pid list: %*
     for %%a in (%*) do @call set "xpids=/pid %%a %%xpids%%"
-    @if not "%xpids%" == "" echo taskkill /f %xpids%
-    @goto :End
-    
-:End
+    if not "%xpids%" == "" taskkill /f %xpids%
+)
+
+@EndLocal
